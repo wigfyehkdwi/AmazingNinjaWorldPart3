a95b4a8e55a2642f51393207b4645bb612d3c05e
austin2011 <austin.murphy.1007@gmail.com>
2024-11-23 12:58:12 -0500

    wt43et

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+        PlayerPrefs.DeleteKey("LIVES_LEFT");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+        // PlayerPrefs.DeleteKey("LIVES_LEFT");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+        // PlayerPrefs.DeleteKey("LIVES_LEFT");
+    }
+}
